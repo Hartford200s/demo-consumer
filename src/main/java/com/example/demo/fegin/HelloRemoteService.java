18e89a3cdee3d4c47058657b6f525d87b55d4e33
Tai <test>
2020-05-13 10:27:35 +0800

    springcloud(四)：熔断器Hystrix (finish) http://www.ityouknow.com/springcloud/2017/05/16/springcloud-hystrix.html

--- src/main/java/com/example/demo/fegin/HelloRemoteService.java
+++ src/main/java/com/example/demo/fegin/HelloRemoteService.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="demo-producer")
+import com.example.demo.hystrix.HelloRemoteServiceHystrix;
+
+@FeignClient(name="demo-producer", fallback = HelloRemoteServiceHystrix.class)
 public interface HelloRemoteService {
 	
 	@RequestMapping(value="/hello")
